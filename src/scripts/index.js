97a515786cd6f30d406b9fb392cb0b94333b95b6
lilavila <lilavila.dev@gmail.com>
2023-08-10 11:58:03 -0300

    correções de funções, botões e acrescimo de modal e responsividade

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,38 +1,105 @@
 import {posts} from "./database.js" 
-import {buttonsLike, buttonsOpenPost} from "./button.js"
 
-function showPosts() {
-    const modal = document.querySelector(".modal");
-    const botton = document.querySelectorAll(".open-post");
+function buttonsLike() {
+    const buttons = document.querySelectorAll(".like");
 
-    for (let i = 0; i < botton.length; i++){
-        botton[i].addEventListener("click", function() {
-            for (let j = 0; j < posts.length; j++){
-                if(Number(botton[i].id) == posts[j].id) {
-                    console.log(posts[j])
+        for (let i = 0; i < buttons.length; i++){
+            buttons[i].addEventListener("click", function() {
+                console.log(buttons[i].src);
+
+                if (buttons[i].src == 'http://127.0.0.1:5500/src/assets/img/gray-heart.svg'){
+                 buttons[i].src = 'http://127.0.0.1:5500/src/assets/img/red-heart.svg'
+                }else {
+                    buttons[i].src = 'http://127.0.0.1:5500/src/assets/img/gray-heart.svg'
                 }
-            }
-        })
-    }
-} 
-showPosts()
-
-function renderPost(post) {
-    const container = document.querySelector(".modal__container");
-
-    post.name;
-    post.profission;
-    post.tilte;
-    post.content;
+            })
+        }    
 }
 
-function addElement() {
-    const newDiv = document.createElement("div");
+function buttonsOpenPost() {
+    const buttons = document.querySelectorAll(".open--post");
 
-    const newH2 = document.createElemen("h2");
+    const modalPost = document.querySelector(".modal__controller");
+    
+        for (let i = 0; i < buttons.length; i++){
+            buttons[i].addEventListener("click", function(event) {
+               const postId = event.target.id;
 
-    const newP = document.createElement("p");
+               for (let j = 0; j < posts.length; j++) {
+                if(posts[j].id == postId){
+                    const modalCreated = createCard(posts[j])
+                   
+                    modalPost.append(modalCreated)
+                    modalPost.showModal()
+                }
+               }
+            })
+        }    
 }
+
+function createCard(element){
+    const divArticle = document.createElement("article")
+    divArticle.setAttribute("id", "article__modal")
+    const divImage = document.createElement("div")
+    const itemImage = document.createElement("img");
+    const itemUser = document.createElement("h2");
+    const divTitle = document.createElement("div")
+    const itemOffice = document.createElement("p");
+    const itemTitle = document.createElement("h2");
+    const itemContent = document.createElement("p");
+
+    itemImage.classList.add("image");
+    itemImage.src = element.img;
+    itemImage.alt = element.user;
+
+    itemUser.classList.add("user__name");
+    itemUser.innerText = element.user;
+
+    itemOffice.classList.add("office");
+    itemOffice.innerText = element.stack;
+
+    itemTitle.classList.add("post__title");
+    itemTitle.innerText = element.title;
+
+    itemContent.classList.add("content__post");
+    itemContent.innerText = element.text;
+
+    divImage.append(itemUser, itemOffice);
+    divTitle.append(itemImage, divImage);
+
+    divArticle.append(divTitle, itemTitle, itemContent);
+
+    return divArticle;
+    
+}
+
+function handleModal() {
+    const modalController = document.querySelector(".modal__controller");
+    const button = document.querySelector(".open--post");
+
+    button.addEventListener("click", function(){
+        modalController.showModal()
+    });
+}
+
+function closeModal() {
+    const modalController = document.querySelector(".modal__controller");
+    const closeButton = document.querySelector(".modal__id");
+    
+    closeButton.addEventListener("click", function(){
+        const article__modal = document.getElementById("article__modal")
+
+        article__modal.remove();
+        modalController.close();
+    })
+}
+
 
 buttonsLike();
 buttonsOpenPost();
+handleModal();
+closeModal();
+
+
+
+
